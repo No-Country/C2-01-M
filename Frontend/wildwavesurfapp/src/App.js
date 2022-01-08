9bfb45482cb87b866f1a73d4cfd98e6541ad22f8
Fide <72738638+fidelp27@users.noreply.github.com>
2022-01-08 09:58:41 -0300

    landing page

--- Frontend/wildwavesurfapp/src/App.js
+++ Frontend/wildwavesurfapp/src/App.js
@@ -1,10 +1,12 @@
 import "./App.css"
 import Header from "./components/header"
+import Landing from "./components/landing/landing"
 
 function App() {
   return (
     <div className='App'>
       <Header />
+      <Landing />
     </div>
   )
 }
---
+++ Frontend/wildwavesurfapp/src/components/landing/landing.jsx
@@ -0,0 +1,40 @@
+import React from "react"
+import Slider from "react-slick"
+
+// styles
+import "./landing.css"
+
+const PromotionalBar = () => {
+  const settings = {
+    dots: false,
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    speed: 3000,
+    autoplaySpeed: 5000,
+    cssEase: "linear",
+
+  }
+  const info = [
+    "https://i.imgur.com/oh7CSuX.jpg",
+    "https://i.imgur.com/uTZanWT.jpg",
+    "https://i.imgur.com/tvUL9LX.jpg",
+  ]
+  return (
+    <div>
+      <Slider {...settings}>
+        {info &&
+          info.map((item, index) => {
+            return (
+              <div key={index} className="img-container">
+                <img src={item} className={"img"+index} alt={"img"+index} />
+              </div>
+            )
+          })}
+      </Slider>
+    </div>
+  )
+}
+
+export default PromotionalBar
