4e07349096ed9325cb53cf8deea609350fcc7b1c
FerRom1979 <fernando_0135@hotmail.com>
2022-01-07 23:01:39 -0300

    feat: create footer component

---
+++ Frontend/wildwavesurfapp/src/components/footer/index.jsx
@@ -0,0 +1,30 @@
+import React from "react"
+import facebook from "../../assets/facebook.svg"
+import instagram from "../../assets/intagram.svg"
+import linkedin from "../../assets/linkedin.svg"
+import twitter from "../../assets/twitter.svg"
+
+// styles
+import { WrapperFooter } from "./Footer.Styles"
+
+const Footer = () => {
+  const social = [facebook, instagram, linkedin, twitter]
+  return (
+    <WrapperFooter>
+      <h6>
+        © Copyright 2021, Wildwavesurf, open source. Todos los derechos
+        reservados.
+      </h6>
+      <div style={{ display: "flex" }}>
+        <h4>FOLLOW US</h4>
+        {social.length > 0
+          ? social.map((item, index) => {
+              return <img src={item} alt={item} key={index} />
+            })
+          : null}
+      </div>
+    </WrapperFooter>
+  )
+}
+
+export default Footer
