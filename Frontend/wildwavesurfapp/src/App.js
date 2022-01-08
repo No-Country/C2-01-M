ff5fddae00c91cbe0e84893130dbc6d5570309d0
Fide <72738638+fidelp27@users.noreply.github.com>
2022-01-08 11:33:48 -0300

    landing added

--- Frontend/wildwavesurfapp/src/App.js
+++ Frontend/wildwavesurfapp/src/App.js
@@ -1,10 +1,14 @@
 import "./App.css"
 import ManagementRoute from "./components/management-route"
+import Header from "./components/header"
+import Landing from "./components/landing/landing"
 
 function App() {
   return (
     <div className='App'>
       <ManagementRoute />
+      <Header />
+      <Landing />
     </div>
   )
 }
