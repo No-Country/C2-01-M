71db47820318360aa7cec5374f2e689c6f57d5f1
FerRom1979 <fernando_0135@hotmail.com>
2022-01-08 16:15:26 -0300

    feat: change in route landing

--- Frontend/wildwavesurfapp/src/App.js
+++ Frontend/wildwavesurfapp/src/App.js
@@ -1,14 +1,10 @@
 import "./App.css"
 import ManagementRoute from "./components/management-route"
-import Header from "./components/header"
-import Landing from "./components/landing/landing"
 
 function App() {
   return (
     <div className='App'>
       <ManagementRoute />
-      <Header />
-      <Landing />
     </div>
   )
 }
--- Frontend/wildwavesurfapp/src/pages/Home.jsx
+++ Frontend/wildwavesurfapp/src/pages/Home.jsx
@@ -1,11 +1,13 @@
 import React from "react"
 import Footer from "../components/footer"
 import Header from "../components/header"
+import Landing from "../components/landing/landing"
 
 const Home = () => {
   return (
     <div>
       <Header />
+      <Landing />
       <Footer />
     </div>
   )
