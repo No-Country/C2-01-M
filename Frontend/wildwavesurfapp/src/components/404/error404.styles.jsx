24274197771e7e31a20a1b46fc9f2c90d1417f58
EdisAnthony <edisanthony@gmail.com>
2022-01-09 09:26:55 -0600

    fix 404 component

---
+++ Frontend/wildwavesurfapp/src/components/404/error404.styles.jsx
@@ -0,0 +1,14 @@
+import styled from "styled-components"
+
+export const Style404 = styled.div`
+.image-container-404{
+	display:grid;
+	place-items:center;
+}
+
+.image-container-404 img{
+		display:inline-block;
+		width:500px;
+		max-width:70vw;
+}
+`
