a5d5dd062925cc3b160f7910088dd771dd615b73
EdisAnthony <edisanthony@gmail.com>
2022-01-11 19:50:15 -0600

    404 image centered

--- Frontend/wildwavesurfapp/src/components/404/error404.styles.jsx
+++ Frontend/wildwavesurfapp/src/components/404/error404.styles.jsx
@@ -4,11 +4,15 @@ export const Style404 = styled.div`
 .image-container-404{
 	display:grid;
 	place-items:center;
+	width: 100%;
+	height: 100vh;
+	margin: 0;
+	padding: 0;
 }
 
 .image-container-404 img{
 		display:inline-block;
 		width:500px;
-		max-width:70vw;
+		max-width:80vw;
 }
 `
