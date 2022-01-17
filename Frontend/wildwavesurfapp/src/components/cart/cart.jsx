6f4c061886d46f7603f078604e9c959b3e0a160a
FerRom1979 <fernando_0135@hotmail.com>
2022-01-17 15:36:01 -0300

    fix: fix bug rebase

--- Frontend/wildwavesurfapp/src/components/cart/cart.jsx
+++ Frontend/wildwavesurfapp/src/components/cart/cart.jsx
@@ -1,5 +1,5 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React from "react"
+import { Link } from "react-router-dom"
 import {
   useCartItem,
   useDeleteFromCart,
@@ -7,13 +7,12 @@ import {
   useTotalCart,
   useDeleteAllItems,
   useCounterItems,
-} from "../../context/ProductContext";
-import add from "../../assets/add.svg";
-import desc from "../../assets/desc.svg";
-import "./cart.css";
+} from "../../context/ProductContext"
+import add from "../../assets/add.svg"
+import desc from "../../assets/desc.svg"
+import "./cart.css"
 
 const Cart = () => {
-
   const cartItem = useCartItem()
   const deleteFromCart = useDeleteFromCart()
   const totalCart = useTotalCart()
@@ -21,17 +20,14 @@ const Cart = () => {
   const deleteAllItems = useDeleteAllItems()
   const counterItems = useCounterItems()
 
-
   const deleteItems = () => {
     const confirm = window.confirm(
       "Are you sure you want to delete everything? This action has no turning back "
-    );
-    if (confirm) deleteAllItems();
-  };
+    )
+    if (confirm) deleteAllItems()
+  }
 
   return cartItem.length === 0 ? (
-
-=======
     <div className='empty-cart'>
       <h3>You have not added products to your cart </h3>
       <Link to='/products'> Products </Link>
@@ -41,13 +37,12 @@ const Cart = () => {
       <div className='shop-detail'>
         <div className='cart-title'>
           <h2>Order detail </h2>
-
         </div>
         {cartItem?.map((item) => {
-          const { image, title, price, _id, cantidad } = item;
-          console.log(item);
+          const { image, title, price, _id, cantidad } = item
+          console.log(item)
           return (
-            <div key={_id} className="card-container-cart" aria-current={true}>
+            <div key={_id} className='card-container-cart' aria-current={true}>
               <img src={image} alt={title} />
               <p>{title} </p>
               <span>Price: ${price} </span>
@@ -63,7 +58,7 @@ const Cart = () => {
                 {cantidad > 1 && (
                   <img
                     src={desc}
-                    alt="add"
+                    alt='add'
                     style={{
                       width: "12px",
                       display: "inline",
@@ -76,7 +71,7 @@ const Cart = () => {
                 {cantidad}
                 <img
                   src={add}
-                  alt="add"
+                  alt='add'
                   style={{
                     width: "12px",
                     display: "inline",
@@ -86,36 +81,32 @@ const Cart = () => {
                 />
               </span>
               <img
-                src="https://i.imgur.com/h67XBJJ.png"
-                alt="garbage"
-                className="icon-delete"
+                src='https://i.imgur.com/h67XBJJ.png'
+                alt='garbage'
+                className='icon-delete'
                 onClick={() => deleteFromCart(item)}
               />
             </div>
-          );
+          )
         })}
       </div>
-
 
       <div className='check-container'>
         <div className='check-title'>
           <h2>Summary </h2>
-
         </div>
-        <div className="check-data">
+        <div className='check-data'>
           <h3>Total Items: {ItemQty} </h3>
-
 
           <h3> Total Buy: ${totalCart} </h3>
           <button>Initial Buy</button>
           {<Link to='/products'>Keep buying</Link>}
-
         </div>
-        <div className="button-delete-all">
+        <div className='button-delete-all'>
           <button onClick={() => deleteItems()}>CLEAN CART</button>
         </div>
       </div>
     </div>
-  );
-};
-export default Cart;
+  )
+}
+export default Cart
--- Frontend/wildwavesurfapp/src/components/item/itemListContainer.jsx
+++ Frontend/wildwavesurfapp/src/components/item/itemListContainer.jsx
@@ -1,58 +1,48 @@
-import React, { useState, useEffect } from "react";
-import ItemList from "./itemList";
-import { useProducts } from "../../context/ProductContext";
-import Sort from "../sort/sort";
-<<<<<<< Updated upstream
-import Filter from "../filter/filter";
+import React, { useState, useEffect } from "react"
+import ItemList from "./itemList"
+import { useProducts } from "../../context/ProductContext"
+import Sort from "../sort/sort"
+import Filter from "../filter/filter"
 
 const ItemListContainer = ({ featured, list, option }) => {
-=======
-
-const ItemListContainer = ({ featured }) => {
->>>>>>> Stashed changes
-  const [items, setItems] = useState([]);
-  const products = useProducts();
-  const [loader, setLoader] = useState(true);
+  const [items, setItems] = useState([])
+  const products = useProducts()
+  const [loader, setLoader] = useState(true)
 
   useEffect(() => {
-    setLoader(true);
+    setLoader(true)
     const getProducts = new Promise((resolve) => {
       setTimeout(() => {
-        resolve(products);
-      }, 1000);
-    });
+        resolve(products)
+      }, 1000)
+    })
     getProducts
       .then((res) => {
-        setItems(res);
+        setItems(res)
       })
       .finally(() => {
-        setLoader(false);
-      });
-  }, [products]);
+        setLoader(false)
+      })
+  }, [products])
 
   return loader ? (
-    <div className="preloader-container">
+    <div className='preloader-container'>
       <img
-        className="preloader"
-        src="https://i.imgur.com/NTByPHS.gif"
-        alt="gif"
+        className='preloader'
+        src='https://i.imgur.com/NTByPHS.gif'
+        alt='gif'
       />
     </div>
   ) : (
     <>
-<<<<<<< Updated upstream
-      <div className="sort-container">
+      <div className='sort-container'>
         <Sort setItems={setItems} />
       </div>
-      <div className="product-container">
+      <div className='product-container'>
         <Filter />
         <ItemList items={items} featured={featured} />
       </div>
-=======
-      <Sort />
-      <ItemList items={items} featured={featured} />
->>>>>>> Stashed changes
     </>
-  );
-};
-export default ItemListContainer;
+  )
+}
+export default ItemListContainer
