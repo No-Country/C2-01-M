b67e28bd0d1fe51202052734862695e1e75a69be
FerRom1979 <fernando_0135@hotmail.com>
2022-01-18 16:45:00 -0300

    feat: add page wish list

--- Frontend/wildwavesurfapp/src/components/item-details/itemDetailContainer.jsx
+++ Frontend/wildwavesurfapp/src/components/item-details/itemDetailContainer.jsx
@@ -1,46 +1,48 @@
-import React, { useState, useEffect } from "react";
-import { useParams } from "react-router-dom";
-import { useAddToCart, useProducts } from "../../context/ProductContext";
-import ItemDetails from "./ItemDetails";
+import React, { useState, useEffect } from "react"
+import { useParams } from "react-router-dom"
+import { useAddToCart, useProducts } from "../../context/ProductContext"
+import { removeFavorites } from "../../helpers/remove-favorites"
+import ItemDetails from "./ItemDetails"
 
 const ItemDetailContainer = () => {
-  const [product, setProduct] = useState([]);
-  const { prodId } = useParams();
-  const products = useProducts();
-  const [loader, setLoader] = useState(true);
-  const [goCart, setGoCart] = useState(false);
-  const addToCart = useAddToCart();
+  const [product, setProduct] = useState([])
+  const { prodId } = useParams()
+  const products = useProducts()
+  const [loader, setLoader] = useState(true)
+  const [goCart, setGoCart] = useState(false)
+  const addToCart = useAddToCart()
 
   useEffect(() => {
-    setLoader(true);
+    setLoader(true)
     const getProduct = new Promise((resolve) => {
       setTimeout(() => {
-        resolve(products);
-      }, 1000);
-    });
+        resolve(products)
+      }, 1000)
+    })
 
     getProduct
       .then((res) => {
         prodId
           ? setProduct(res.find((item) => item._id === prodId))
-          : setProduct(res);
+          : setProduct(res)
       })
       .finally(() => {
-        setLoader(false);
-      });
-  }, [prodId, products]);
+        setLoader(false)
+      })
+  }, [prodId, products])
 
   const onAdd = (cantidad) => {
-    addToCart(product, cantidad);
-    setGoCart(true);
-  };
+    addToCart(product, cantidad)
+    setGoCart(true)
+    removeFavorites(product?._id)
+  }
 
   return loader ? (
-    <div className="preloader-container">
+    <div className='preloader-container'>
       <img
-        className="preloader"
-        src="https://i.imgur.com/NTByPHS.gif"
-        alt="gif"
+        className='preloader'
+        src='https://i.imgur.com/NTByPHS.gif'
+        alt='gif'
       />
     </div>
   ) : (
@@ -54,6 +56,6 @@ const ItemDetailContainer = () => {
     >
       <ItemDetails product={product} onAdd={onAdd} goCart={goCart} />
     </div>
-  );
-};
-export default ItemDetailContainer;
+  )
+}
+export default ItemDetailContainer
