64e528db60de9f061fe2880f04877aa9431a039c
brahianpdev <zk.spywn@gmail.com>
2022-01-18 16:47:50 -0300

    Refactor: Code optimization

--- Backend/index.js
+++ Backend/index.js
@@ -5,12 +5,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 
 const { config } = require("./config");
-
-const authRoutes = require('./routes/auth.route');
-const usersRoutes = require('./routes/users.route');
-const producstRoutes = require('./routes/products.route');
-const categoriesRoutes = require('./routes/categories.route');
-const cartroute = require('./routes/cart.route');
+const indexRoute = require('./routes');
 
 mongoose
   .connect(config.db.mongo)
@@ -28,11 +23,7 @@ mongoose
     app.use(morgan("dev"));
     app.use(cors());
 
-    app.use('/', authRoutes);
-    app.use('/users', usersRoutes);
-    app.use('/products', producstRoutes);
-    app.use('/categories', categoriesRoutes);
-    app.use('/cart', cartroute);
+    app.use('/', indexRoute);
 
     app.listen(config.app.port, () => {
       console.log(`🔥 Server is running at port ${config.app.port}`);
