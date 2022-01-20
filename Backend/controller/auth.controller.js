92b42343029a3bb00a09614c810125d93b4a4cff
brahianpdev <zk.spywn@gmail.com>
2022-01-20 16:09:49 -0300

    Feat: Auth Service

--- Backend/controller/auth.controller.js
+++ Backend/controller/auth.controller.js
@@ -1,30 +1,23 @@
 const bcrypt = require("bcrypt");
 
 const User = require("../models/user.model");
-const generateJWT = require('../middelwares/generateJWT');
+const generateJWT = require("../middelwares/generateJWT");
+const AuthService = require("../services/auth.service");
 
 class authController {
   async register(req, res) {
     try {
       const { name, email, password, role } = req.body;
-
       const user = new User({ name, email, password, role });
 
-      const salt = bcrypt.genSaltSync();
-      user.password = bcrypt.hashSync(password, salt);
-
-      const newUser = await user.save();
+      const userData = await new AuthService().authRegister(user, password);
 
       return res.status(200).json({
         message: "Registered user successfully",
-        newUser,
+        userData,
       });
-
     } catch (error) {
-      res.status(500).json({
-        message: "Failed. Please try again.",
-        error,
-      });
+      res.status(500).json(error);
     }
   }
 
@@ -34,32 +27,14 @@ class authController {
     try {
       const user = await User.findOne({ email });
 
-      if (!user) {
-        return res.status(400).json({
-          message: "User and or password is incorrect",
-        });
-      }
-
-      if (!user.state) {
-        return res.status(400).json({
-          message: "User and or password is incorrect",
-        });
-      }
-
-      const validPassword = bcrypt.compareSync(password, user.password);
-      if (!validPassword) {
-        return res.status(400).json({
-          message: "User and or password is incorrect",
-        });
-      }
-
+      await new AuthService().authLogin(email, password);
       const token = await generateJWT(user.id);
 
       res.json({
+        message: "Logged in successfully",
         user,
         token,
       });
-      
     } catch (error) {
       console.log(error);
       res.status(500).json({
