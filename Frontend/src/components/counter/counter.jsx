4d466d11910c071d9e4d28589b2ff2f867e6b4e4
FerRom1979 <fernando_0135@hotmail.com>
2022-01-21 11:29:36 -0300

    fix: add styles a the buttons item details

--- Frontend/src/components/counter/counter.jsx
+++ Frontend/src/components/counter/counter.jsx
@@ -1,25 +1,39 @@
-import React, {useState} from "react";
+import React, { useState } from "react"
 import "./counter.css"
 
-const Counter = ({onAdd})=>{
-    const[number, setNumber] = useState(0)
-    
-    const sumNumber =()=>{
-        setNumber(number + 1)
-    }
-    const subsNumber =()=>{
-        setNumber(number - 1)
-    }
-    
-    return(
-        
-        <div>
-            <button className="btn-counter" onClick={()=>subsNumber()} disabled={number===0}><span>-</span></button>
-            <p className="counter-number"><span>{number}</span></p>
-            <button className="btn-counter" onClick={()=>sumNumber()}><span>+</span></button>
-            <button className="btn-add-to-cart" onClick={()=>onAdd(number)} disabled={number===0}>Add to cart</button>
-        </div>
+const Counter = ({ onAdd }) => {
+  const [number, setNumber] = useState(0)
 
-    )
+  const sumNumber = () => {
+    setNumber(number + 1)
+  }
+  const subsNumber = () => {
+    setNumber(number - 1)
+  }
+
+  return (
+    <div className='container-counter'>
+      <button
+        className='btn-counter'
+        onClick={() => subsNumber()}
+        disabled={number === 0}
+      >
+        <span>-</span>
+      </button>
+      <p className='counter-number'>
+        <span>{number}</span>
+      </p>
+      <button className='btn-counter' onClick={() => sumNumber()}>
+        <span>+</span>
+      </button>
+      <button
+        className='btn-add-to-cart'
+        onClick={() => onAdd(number)}
+        disabled={number === 0}
+      >
+        Add to cart
+      </button>
+    </div>
+  )
 }
 export default Counter
