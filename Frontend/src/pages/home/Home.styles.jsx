352560f4643e70549187ef340d8f6d43e60090fa
FerRom1979 <fernando_0135@hotmail.com>
2022-01-21 16:28:23 -0300

    feat: create page comments

--- Frontend/src/pages/home/Home.styles.jsx
+++ Frontend/src/pages/home/Home.styles.jsx
@@ -13,3 +13,22 @@ export const SurfboardsTitle = styled.span`
   font-size: 40px;
   font-weight: bold;
 `
+export const WrapperComments = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  p {
+    font-size: 24px;
+    margin: 0;
+  }
+  @media (max-width: 690px) {
+    flex-direction: column;
+  }
+`
+
+export const WrapperImg = styled.div`
+  display: flex;
+  justify-content: ${({ right }) => (right ? "flex-end" : "flex-start")};
+  width: 100%;
+  margin-left: ${({ right }) => (right ? "-20px" : "20px")};
+`
