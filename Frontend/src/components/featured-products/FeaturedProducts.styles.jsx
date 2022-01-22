693310c07c9d5f26abae2117dee8206478d6e1a9
Fide <72738638+fidelp27@users.noreply.github.com>
2022-01-22 14:13:48 -0300

    estilos y responsive pagina principal

--- Frontend/src/components/featured-products/FeaturedProducts.styles.jsx
+++ Frontend/src/components/featured-products/FeaturedProducts.styles.jsx
@@ -1,14 +1,30 @@
-import styled from "styled-components"
+import styled from "styled-components";
 
 export const Wrapper = styled.div`
   display: flex;
   flex-direction: column;
   justify-content: center;
+  height: 100vh;
+  width: 100vw;
+  max-height: 100%;
   text-align: center;
+  margin 0;
+  background-image: url("https://i.imgur.com/GjKOrVm.jpg");
+  background-position: center;
+  background-repeat: none;
+  background-size: cover;
+
   h1 {
-    font-size: 30px;
+    font-size: 1.5rem;
     font-weight: 600;
     color: #060606;
     font-family: "Oswald", sans-serif;
+    margin-top: 1rem;
+    letter-spacing: 3px;
   }
-`
+  @media (max-width: 690px) {
+    flex-direction: column;
+    height: 100%;
+    
+  }
+`;
--- Frontend/src/components/sort/sort.jsx
+++ Frontend/src/components/sort/sort.jsx
@@ -1,7 +1,7 @@
-import React, { useState, useEffect } from "react"
-import { useProducts } from "../../context/ProductContext"
-import Select from "react-select"
-import "./sort.css"
+import React, { useState, useEffect } from "react";
+import { useProducts } from "../../context/ProductContext";
+import Select from "react-select";
+import "./sort.css";
 
 const Sort = ({ setItems }) => {
   const options = [
@@ -9,17 +9,17 @@ const Sort = ({ setItems }) => {
     { value: "Z-A", label: "Z - A" },
     { value: "LowPrice", label: "Low Price" },
     { value: "HighPrice", label: "High Price" },
-  ]
+  ];
 
-  const [option, setOption] = useState(null)
-  const products = useProducts()
+  const [option, setOption] = useState(null);
+  const products = useProducts();
 
   const changeOption = (evt) => {
-    setOption(evt.value)
-  }
+    setOption(evt.value);
+  };
 
   const sortProducts = () => {
-    let sortedList
+    let sortedList;
     switch (option) {
       case "LowPrice":
         sortedList = [...products].sort((a, b) =>
@@ -28,8 +28,8 @@ const Sort = ({ setItems }) => {
             : parseInt(a.price) < parseInt(b.price)
             ? -1
             : 0
-        )
-        break
+        );
+        break;
       case "HighPrice":
         sortedList = [...products].sort((a, b) =>
           parseInt(a.price) > parseInt(b.price)
@@ -37,34 +37,34 @@ const Sort = ({ setItems }) => {
             : parseInt(a.price) < parseInt(b.price)
             ? 1
             : 0
-        )
-        break
+        );
+        break;
       case "A-Z":
         sortedList = [...products].sort((a, b) =>
           a.title > b.title ? 1 : a.title < b.title ? -1 : 0
-        )
-        break
+        );
+        break;
       case "Z-A":
         sortedList = [...products].sort((a, b) =>
           a.title > b.title ? -1 : a.title < b.title ? 1 : 0
-        )
-        break
+        );
+        break;
       default:
-        sortedList = products
+        sortedList = products;
     }
 
-    setItems(sortedList)
-  }
+    setItems(sortedList);
+  };
 
   useEffect(() => {
-    sortProducts()
-  }, [option])
+    sortProducts();
+  }, [option]);
 
   return (
-    <div className='sort-container'>
+    <div className="sort-container">
       <Select options={options} onChange={changeOption} option={option} />
     </div>
-  )
-}
+  );
+};
 
-export default Sort
+export default Sort;
